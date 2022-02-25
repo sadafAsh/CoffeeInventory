fd6e4755eeb83088335e45d29dcb2246c1a4d95f
test@gmail.com <test@gmail.com>
2022-02-25 23:07:08 +0000

    first commit

--- src/main/java/com/soj/coffee/inventory/CoffeeInventoryApplication.java
+++ src/main/java/com/soj/coffee/inventory/CoffeeInventoryApplication.java
@@ -4,7 +4,7 @@ import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-
+//first comment
 @SpringBootApplication
 public class CoffeeInventoryApplication {
 static final Logger logger= LogManager.getLogger(CoffeeInventoryApplication.class.getName());
